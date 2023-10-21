97194944ce9961cb0306fb5016af0bcbea24756c
Hiroki.T <hirodora@me.com>
2023-10-22 02:28:53 +0900

    add: user page

--- src/haiku.py
+++ src/haiku.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import db
 
 app = Flask(__name__)
@@ -6,11 +6,20 @@ app = Flask(__name__)
 db.Base.metadata.create_all(bind=db.engine)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/", methods=["GET"])
+def root():
+    return render_template("root.html")
 
 
-@app.route("/post")
+@app.route("/post", methods=["GET", "POST"])
 def post():
-    return render_template("post.html")
+    if request.method == "GET":
+        return render_template("post.html")
+    else:
+        # POST
+        pass
+
+
+@app.route("/<username>", methods=["GET"])
+def index(username: str):
+    return render_template("index.html", name=username)
