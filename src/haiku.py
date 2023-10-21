f880843427a224b817c50ca7f4d698f45174a899
Hiroki.T <hirodora@me.com>
2023-10-22 03:08:17 +0900

    add: some html files

--- src/haiku.py
+++ src/haiku.py
@@ -14,12 +14,26 @@ def root():
 @app.route("/post", methods=["GET", "POST"])
 def post():
     if request.method == "GET":
+        # TODO: ログインされていなかったら、/loginにリダイレクト
         return render_template("post.html")
     else:
         # POST
+        # TODO: フロントから受け取った俳句を、データベースに保存する
+        pass
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        # POST
+        # TODO: パスワードが合っていたら、cookieをset
         pass
 
 
 @app.route("/<username>", methods=["GET"])
 def index(username: str):
+    # TODO: データベースからユーザーが過去に投稿した俳句を抜き出し、レンダリング
+
     return render_template("index.html", name=username)
